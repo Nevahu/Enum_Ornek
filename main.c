5aa2a1d870444cb63e0e7496942876879d4044b8
Nevahu <nevahuarslan26@gmail.com>
2022-08-25 23:26:57 +0300

    Enum_Ornek

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+enum sanfoundry
+{
+    c=0, d=10, h=20, s=3
+}a;
+int main()
+{
+    a =c;
+    printf("Size of enum variable = %d bytes", sizeof(a));
+    return 0;
+}
+//enum hafýzada 4 byt yer kaplar.
